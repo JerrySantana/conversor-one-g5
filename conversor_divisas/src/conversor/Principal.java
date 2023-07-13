da5cc706a7b82d22a30d137a3db8844f6ed2f8bf
Gerardo Santana <91979984+JerrySantana@users.noreply.github.com>
2023-07-12 20:33:14 -0600

    Añadid Javadoc al main de la aplicacion

--- conversor_divisas/src/conversor/Principal.java
+++ conversor_divisas/src/conversor/Principal.java
@@ -2,6 +2,11 @@ package conversor;
 
 import gui.VentanaInicio;
 
+/**
+ * Clase principal desde la cual se inicia la aplicacion.
+ * @author Gerardo Santana
+ * @version 1.0
+ */
 public class Principal {
 	public static void main(String[] args) {
 		// Ejecutamos la aplicación.
